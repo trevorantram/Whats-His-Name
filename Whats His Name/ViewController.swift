d2384443e68eb8dd78b3b2fd70b08e9386ddaa39
Trevor Antram <trevor.antram@outlook.com>
2016-08-14 11:31:16 -0400

    first commit

---
+++ Whats His Name/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats His Name
+//
+//  Created by Trevor Antram on 8/13/16.
+//  Copyright © 2016 T&Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
